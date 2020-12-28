a56c28fe17b0a8635ee0a8ade9fdb3fafedd6ddd
lv-cha <zhaoxicn@foxmail.com>
2020-12-28 16:17:51 +0800

    run

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ from zipline import run_algorithm
 import pandas as pd
 
 
-def initialize():
-    print("intialize")
+def initialize(*args, **kwargs):
+    print(args)
+    print(kwargs)
+    print("initialize")
 
 
 if __name__ == "__main__":
     run_algorithm(
-        start=pd.Timestamp(datetime(year=2019, month=11, day=1)),
-        end=pd.Timestamp(datetime(year=2019, month=11, day=2)),
+        start=pd.Timestamp(datetime(year=2019, month=11, day=1), tz="UTC"),
+        end=pd.Timestamp(datetime(year=2019, month=11, day=2), tz="UTC"),
         initialize=initialize,
         capital_base=1000000
     )
